9d69c27e701939ed33ea6a23b1310a17748d7499
Joshua Stowers <stowers.joshua@live.com>
2015-11-23 14:32:31 -0600

    Added readers for all attributes in Record This permits us to do Record.first.name

--- lib/splicer/dns_made_easy/record.rb
+++ lib/splicer/dns_made_easy/record.rb
@@ -2,13 +2,21 @@ module Splicer
   module DnsMadeEasy
 
     class Record
-      attr_reader :id, :type, :value, :ttl, :data
+      attr_reader :name, :value, :id, :type, :dynamicDns,
+                  :failed, :gtdLocation, :hardLink, :ttl,
+                  :failover, :monitor, :sourceId, :source, :data
+
       def initialize(params={})
         @id = params['id']
         @type = params['type']
         @value = params['value']
         @ttl = params['ttl']
         @data = params
+      end
+
+      def add_attribute(attribute, value)
+        attribute = "@#{attribute}"
+        self.instance_variable_set(attribute, value)
       end
 
       def persisted?
