32126c3b195b60ced1c89626fec9977e10c1ec72
Oliver Garcia <ogarci5@gmail.com>
2016-09-13 15:17:46 -0500

    Changed Dynect to DNS Made Easy

--- spec/splicer/dns_made_easy/client_spec.rb
+++ spec/splicer/dns_made_easy/client_spec.rb
@@ -11,12 +11,12 @@ describe Splicer::DnsMadeEasy::Client do
   describe '#post' do
     subject { client.post('resource', { data: 'something' }) }
 
-    context 'when Dynect returns with success' do
+    context 'when DNS Made Easy returns with success' do
       before { client.stub(:execute).and_return({}) }
       it { should eq({})}
     end
 
-    context 'when Dynect returns with an error' do
+    context 'when DNS Made Easy returns with an error' do
       before { client.stub(:execute).and_raise(Splicer::Errors::RequestError) }
       it 'should raise a Splicer::Errors::RequestError' do
         expect { subject }.to raise_error(Splicer::Errors::RequestError)
@@ -27,12 +27,12 @@ describe Splicer::DnsMadeEasy::Client do
   describe '#get' do
     subject { client.get('resource') }
 
-    context 'when Dynect returns with success' do
+    context 'when DNS Made Easy returns with success' do
       before { client.stub(:execute).and_return({}) }
       it { should eq({})}
     end
 
-    context 'when Dynect returns with an error' do
+    context 'when DNS Made Easy returns with an error' do
       before { client.stub(:execute).and_raise(Splicer::Errors::RequestError) }
       it 'should raise a Splicer::Errors::RequestError' do
         expect { subject }.to raise_error(Splicer::Errors::RequestError)
@@ -43,12 +43,12 @@ describe Splicer::DnsMadeEasy::Client do
   describe '#put' do
     subject { client.put('resource', { data: 'something' }) }
 
-    context 'when Dynect returns with success' do
+    context 'when DNS Made Easy returns with success' do
       before { client.stub(:execute).and_return({}) }
       it { should eq({})}
     end
 
-    context 'when Dynect returns with an error' do
+    context 'when DNS Made Easy returns with an error' do
       before { client.stub(:execute).and_raise(Splicer::Errors::RequestError) }
       it 'should raise a Splicer::Errors::RequestError' do
         expect { subject }.to raise_error(Splicer::Errors::RequestError)
@@ -59,12 +59,12 @@ describe Splicer::DnsMadeEasy::Client do
   describe '#delete' do
     subject { client.delete('resource') }
 
-    context 'when Dynect returns with success' do
+    context 'when DNS Made Easy returns with success' do
       before { client.stub(:execute).and_return({}) }
       it { should eq({})}
     end
 
-    context 'when Dynect returns with an error' do
+    context 'when DNS Made Easy returns with an error' do
       before { client.stub(:execute).and_raise(Splicer::Errors::RequestError) }
       it 'should raise a Splicer::Errors::RequestError' do
         expect { subject }.to raise_error(Splicer::Errors::RequestError)
